608527ad7429182c47cf7e2cf1e174af9bc754d3
Grant Coble-Neal <grantcn96@gmail.com>
2022-03-20 16:25:49 +0800

    Initial commit

---
+++ R/getNEMdata.R
@@ -0,0 +1,32 @@
+
+# Objective: get and compile NEM market data
+# Author: Grant Coble-Neal
+library(stringr)
+year <- 2022
+
+prefix <- c("PRICE_AND_DEMAND_")
+region <- c("NSW")
+
+filePath <- c("https://aemo.com.au/aemo/data/nem/priceanddemand/")
+
+subdirectory <- c("data")
+wd <- getwd()
+filePath2 <- file.path(wd, subdirectory, paste0("NEM_", region, "_" ,year, ".rds"))
+datalist = list()
+myMonth = 1
+for (myMonth in 1:2)
+{
+  month <- str_pad(myMonth, 2, "left", pad="0")
+  file <- c(paste0(prefix, year, month, "_", region,"1.csv" ))
+  myURL <- paste0(filePath, file)
+  
+  df <- read.csv(myURL)
+  datalist[[myMonth]] <- df # add to list
+}
+
+# append files
+NEM_data = do.call(rbind, datalist)
+
+# save file
+saveRDS(NEM_data, filePath2)
+
