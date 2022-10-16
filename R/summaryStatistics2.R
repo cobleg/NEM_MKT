85bcebf34d6b1b4ce14c5df34f9ea7b1cd05f76a
Grant Coble-Neal <grantcn96@gmail.com>
2022-10-16 13:41:21 +0800

    summary statistics

--- R/summaryStatistics2.R
+++ R/summaryStatistics2.R
@@ -3,6 +3,8 @@
 # Objective: calculate summary statistics by region over a 12-month period
 # Author: Grant Coble-Neal
 
+library(dplyr)
+
 SummaryStatistics <- df.21_22 %>% 
   group_by(REGION, year) %>% 
   filter(year == 2021) %>% 
@@ -11,7 +13,7 @@ SummaryStatistics <- df.21_22 %>%
     PriceVol = sd(AveragePrice)
   )
 
-sd <- as.numeric(SummaryStatistics[SummaryStatistics$REGION == "NSW1", "PriceVol"])
+sd <- as.numeric(SummaryStatistics[SummaryStatistics$REGION == "QLD1", "PriceVol"])
 
 rand_norms_10000 <- rnorm(n = 10000, mean = 60, sd = sd) # normally distributed prices
 
@@ -20,14 +22,14 @@ hist(rand_norms_10000, xlab = "Random value ($/MWh)", col = "grey",
 
 actualSpotPrices <- df.21_22 %>% 
   group_by(REGION, year) %>% 
-  filter(year == 2021, REGION == "NSW1") %>% 
+  filter(year == 2021, REGION == "QLD1") %>% 
   select( AveragePrice )
 
 hist(actualSpotPrices$AveragePrice, xlab = "$/MWh", col = "grey",
-     main = "Actual Spot Price for NSW 2021", cex.lab = 1.5, cex.axis = 1.5, breaks = "Scott")
+     main = "Actual Spot Price for QLD 2021", cex.lab = 1.5, cex.axis = 1.5, breaks = "Scott")
 
 library(ggplot2)
 ggplot(actualSpotPrices, aes(AveragePrice)) + geom_histogram() + 
   facet_wrap(~(AveragePrice > 300), scale = 'free') +
-  ggtitle('NSW Spot Price 2021 ($/MWh)') + 
+  ggtitle('QLD Spot Price 2021 ($/MWh)') + 
   xlab('Price ($/MWh)')
