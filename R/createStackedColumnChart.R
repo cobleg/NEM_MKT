566cf0839fba976727adf83286fcae6be81abb52
Grant Coble-Neal <grantcn96@gmail.com>
2022-11-11 09:40:23 +0800

    completed script to produce stacked chart of NEM prices

--- R/createStackedColumnChart.R
+++ R/createStackedColumnChart.R
@@ -5,25 +5,43 @@
 
 library(here)
 library(tidyverse)
-df.21_22 <- readRDS(here("data", "NEM_regions_2021.rds")) %>% 
+
+source(here("R", "convertIntervals2.R"))
+
+Price.Bands_Monthly <- df.21_22 %>% 
   mutate(
-    Month = lubridate::month(SETTLEMENTDATE)
+    Month = lubridate::month(SETTLEMENTDATE),
+    Year = lubridate::year(SETTLEMENTDATE)
   ) %>% 
-  group_by(REGION) %>% 
+  mutate(
+    Date = lubridate::make_date(year = Year, month = Month, day = 1)
+  ) %>% 
+  group_by(REGION, Date) %>% 
   mutate(
     Price.Buckets = case_when(
-      RRP <= -500 ~ "<= 500",
-      RRP > -500 & RRP <= 0 ~ "<= 0",
-      RRP > 0 & RRP <= 50 ~ "<= 50",
-      RRP > 50 & RRP <= 100 ~ "<= 100",
-      RRP > 100 & RRP <= 200 ~"<= 200",
-      RRP > 200 & RRP <= 500 ~ "<= 500",
-      RRP > 500 & RRP <= 1000 ~ "<= 1000",
-      RRP > 1000 & RRP <= 2000 ~ "<= 2000",
-      RRP > 2000 & RRP <= 5000 ~ "<= 5000",
-      RRP > 5000 & RRP <= 10000 ~ "<= 10000",
-      RRP > 10000 
+      AveragePrice <= -500 ~ "<= 500",
+      AveragePrice > -500 & AveragePrice <= 0 ~ "<= 0",
+      AveragePrice > 0 & AveragePrice <= 50 ~ "<= 50",
+      AveragePrice > 50 & AveragePrice <= 100 ~ "<= 100",
+      AveragePrice > 100 & AveragePrice <= 200 ~"<= 200",
+      AveragePrice > 200 & AveragePrice <= 500 ~ "<= 500",
+      AveragePrice > 500 & AveragePrice <= 1000 ~ "<= 1000",
+      AveragePrice > 1000 & AveragePrice <= 2000 ~ "<= 2000",
+      AveragePrice > 2000 & AveragePrice <= 5000 ~ "<= 5000",
+      AveragePrice > 5000 & AveragePrice <= 10000 ~ "<= 10000",
+      AveragePrice > 10000 ~ "> 10000"
     )
-  )
-  
+  ) %>% 
+  group_by(REGION, Date, Price.Buckets) %>% 
+  summarise(
+    Count = n()
+  ) 
+
+Price.Bands_Monthly %>% 
+  ggplot(
+    aes(fill = forcats::fct_rev(Price.Buckets), y = Count, x = Date) 
+  ) +
+  geom_bar(position = "stack", stat = "identity") +
+  facet_grid(cols = vars(REGION)) +
+  labs( fill = "Price bucket")
   
