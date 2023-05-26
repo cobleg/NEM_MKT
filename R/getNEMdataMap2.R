f6219e6d113de694d30a81e3435ed90dbd43db5c
Grant Coble-Neal <grantcn96@gmail.com>
2023-05-26 15:55:14 +0800

    Added script to create summary statistics and export to Excel

--- R/getNEMdataMap2.R
+++ R/getNEMdataMap2.R
@@ -42,10 +42,41 @@ NEM_regions_data %>% as_tibble() %>%
     DateTime = lubridate::as_datetime(SETTLEMENTDATE),
     Date = lubridate::as_date(SETTLEMENTDATE),
     Time = format(as.POSIXct(SETTLEMENTDATE), format = "%H:%M:%S")
+  ) %>% 
+  mutate(
+    Year = lubridate::year(Date),
+    Month = lubridate::month(Date)
+  ) %>% 
+  mutate(
+    Season = case_when(
+      Month %in% c(1,2,12) ~ "Summer",
+      Month %in% c(3,4,5) ~ "Autumn",
+      Month %in% c(6,7,8) ~ "Winter",
+      Month %in% c(9,10,11) ~ "Spring",
+      TRUE ~ "NA"
+    )
   ) %>% select(
-    REGION, Date, Time, TOTALDEMAND, RRP, PERIODTYPE
+    REGION, Season, Date, Year, Month, Time, TOTALDEMAND, RRP
   )  -> NEM_regions_data.tb 
 
+# create meta data file
+Meta.Data <- data.frame(
+  "Variable" = c("REGION", "Season", "Date", "Year", "Month", "Time",  "TOTALDEMAND", "RRP"),
+  "UoM" = c(rep(NA,3), "years", "months",  "Hours:Minutes:Seconds", "MW", "$/MW")
+)
+
+# Create summary statistics
+NEM_regions_data.tb %>% pivot_longer(
+  !c(REGION, Season, Date, Year, Month, Time),
+  names_to = "Series",
+  values_to = "Values"
+) %>% group_by(
+  REGION, Season, Year, Month, Time, Series
+) %>% summarise(
+  quantile.values = quantile(Values, c(0, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.975, 0.99, 1)),
+  quantiles = c(0, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.975, 0.99, 1)
+) -> NEM_summary.stats
+  
 # save files
 fileName <- paste0("NEM_regions_", years, ".rds" )
 saveRDS(NEM_regions_data, file.path(wd, subdirectory, fileName))
@@ -54,4 +85,29 @@ map( filePath2, ~saveRDS(NEM_regions_data, .x) )
 
 # export to Excel
 library(openxlsx)
-write.xlsx(NEM_regions_data.tb, file = here("data", "NEM_Data.xlsx"), sheetName = 'Data', rowNames=FALSE, asTable = TRUE)
+wb <- createWorkbook()
+addWorksheet(wb, "Data")
+addWorksheet(wb, "Meta Data")
+
+writeData(wb, "Data", NEM_regions_data.tb)
+writeData(wb, "Meta Data", Meta.Data)
+
+saveWorkbook(wb, file = here("data", "NEM_Data.xlsx"), overwrite = TRUE )
+
+# export summary stats to Excel
+NEM_summary.stats %>% group_by(REGION, Series) %>% 
+  group_split() -> NEM_Summary_List
+
+# Reference: http://optimumsportsperformance.com/blog/r-tips-tricks-write-data-to-separate-excel-sheet-tabs/
+blank_excel <- createWorkbook()
+
+Map(function(df, tab_number)
+  {
+  addWorksheet(blank_excel, paste0("Sheet",tab_number))
+  writeData(blank_excel, paste0("Sheet",tab_number), df)
+  },
+  NEM_Summary_List, 1:length(NEM_Summary_List)
+)
+
+saveWorkbook(blank_excel, file = here("data", "Summary_Stats.xlsx"), overwrite = TRUE)
+
